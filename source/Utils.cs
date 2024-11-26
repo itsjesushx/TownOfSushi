83e5f4277b3992ab6de02ad8577021ad2aaeef5a
Jesushi <140932715+itsjesushx@users.noreply.github.com>
2024-11-26 11:16:22 -0500

    moved some code so there is not that many files

--- source/Utils.cs
+++ source/Utils.cs
@@ -1589,5 +1589,22 @@ namespace TownOfSushi
             var maxDistance = KillDistance();
             return (GetDistBetweenPlayers(player, target) > maxDistance);
         }
+
+        [HarmonyPatch(typeof(AmongUs.Data.Player.PlayerData), nameof(AmongUs.Data.Player.PlayerData.FileName), MethodType.Getter)]
+        public class SaveManagerPatch
+        {
+            public static void Postfix(ref string __result)
+            {
+               __result += "_STOU";
+            }
+        }
+        [HarmonyPatch(typeof(AmongUs.Data.Legacy.LegacySaveManager), nameof(AmongUs.Data.Legacy.LegacySaveManager.GetPrefsName))]
+        public class LegacySaveManagerPatch    
+        {        
+            public static void Postfix(ref string __result)        
+            {            
+                __result += "_STOU";        
+            }    
+        }
     }
 }
