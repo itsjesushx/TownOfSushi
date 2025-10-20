aee8739331833cb6f15e99381aa86e165eb97c5d
Haram Leonard <haram.leonard@gmail.com>
2025-10-19 20:20:38 -0500

    Set Dev status to false for release

--- TownOfSushi/TownOfSushiPlugin.cs
+++ TownOfSushi/TownOfSushiPlugin.cs
@@ -43,11 +43,11 @@ public partial class TownOfSushiPlugin : BasePlugin, IMiraPlugin
     /// <summary>
     ///     Determines if the current build is a dev build or not. This will change certain visuals as well as always grab news locally to be up to date.
     /// </summary>
-    public static bool IsDevBuild => true;
+    public static bool IsDevBuild => false;
     
     /// <inheritdoc />
     public string OptionsTitleText => "Town of Sushi";
-    public const string DevString = " (Dev 20)";
+    public const string DevString = "";
 
     /// <inheritdoc />
     public ConfigFile GetConfigFile()
